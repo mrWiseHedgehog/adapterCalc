b9c89a47399af5c3b8ee66c3e64293ee4fc7a0d4
Leonid Sitnikov <hedgehog@yandex.ru>
2023-03-03 21:15:34 +0300

    added adapter class

--- src/main/java/ru/netology/Ints.java
+++ src/main/java/ru/netology/Ints.java
@@ -1,2 +1,10 @@
-package ru.netology;public interface Ints {
+package ru.netology;
+
+public interface Ints {
+
+    int sum(int arg0, int arg1);
+
+    int mult(int arg0, int arg1);
+
+    int pow(int a, int b);
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,11 @@
-package ru.netology;public class Main {
+package ru.netology;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
     }
 }
